1a3dcbc10e7285bc770817560aa804026afaf097
Darsi Dharani <dharanidarisi@gmail.com>
2025-06-26 16:02:57 +0530

    Added passport dependencies

--- client/src/pages/Order.jsx
+++ client/src/pages/Order.jsx
@@ -12,7 +12,8 @@ export default function Order({ user }) {
         background: "linear-gradient(to right top, #eef2f3, #8ec5fc)",
       }}
     >
-      <div className="relative z-10 w-full max-w-2xl mx-auto p-6 rounded-xl shadow-2xl animate-fade-in">
+     <div className="relative z-10 w-full max-w-5xl mx-auto p-8 rounded-xl shadow-2xl animate-fade-in">
+
         <h1 className="text-3xl font-bold mb-4 text-indigo-700 text-center">
           Create a New Delivery
         </h1>
@@ -21,7 +22,7 @@ export default function Order({ user }) {
           <CreateDelivery
             onClose={() => {
               setRefresh((r) => !r);
-              toast.success("Delivery request created!");
+              
             }}
           />
         ) : (
@@ -31,21 +32,7 @@ export default function Order({ user }) {
         )}
       </div>
 
-      <style jsx>{`
-        @keyframes fade-in {
-          from {
-            opacity: 0;
-            transform: translateY(10px);
-          }
-          to {
-            opacity: 1;
-            transform: translateY(0);
-          }
-        }
-        .animate-fade-in {
-          animation: fade-in 0.6s ease-out;
-        }
-      `}</style>
+     
     </div>
   );
 }
